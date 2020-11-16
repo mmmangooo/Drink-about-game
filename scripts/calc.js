c21a501ebe85ba07a842bdfbab85fa2814ebdbaa
mmmangooo <isabelle.angsas@gmail.com>
2020-11-16 15:04:26 +0000

    Finished challenge, written test and test-driven code for drink-about game

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,5 +1,21 @@
-whatCanIDrink(age); {
-if(age<0) {
-    return "Sorry. I can't tell what drink because that age is incorrect!"
+function whatCanIDrink(age) {
+if(age<=0) {
+    return "Sorry. I can't tell what drink because that age is incorrect!";
 }
+else if (age<14) {
+    return "Drink toddy";
+}
+else if (age<18 && age >=14) {
+    return "Drink coke";
+}
+else if (age<21 && age >=18) {
+    return "Drink beer";
+}
+else if (age<130) {
+return "Drink whiskey";
+}
+else {
+    return "Sorry, I can't tell what to drink because that age is incorrect!";
+}
+
 }
--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,8 +1,34 @@
-describe ("age", function() {
-    describe("return tests", function() {
-        it("Should return Sorry, I can't tell what to drink because that age is incorrect!", function() {
-           whatCanIDrink.age(0);
-           expect(whatCanIDrink.return).toBe("Sorry, I can't tell what to drink because that age is incorrect!"); 
+
+describe ("whatCanIDrink", function() {
+
+    beforeEach(function() {
+        result=new whatCanIDrink; 
+    });
+    describe("checkAge", function() {
+
+        it("should return Sorry, I can't tell what to drink because that age is incorrect!", function() {
+            var result=whatCanIDrink(0);
+           expect(result).toBe("Sorry. I can't tell what drink because that age is incorrect!"); 
+    });
+         it ("should return Drink toddy", function(){
+           var result=whatCanIDrink(13);
+            expect(result).toBe("Drink toddy");
+    });
+        it ("should return Drink coke", function() {
+            var result=whatCanIDrink(17);
+            expect(result).toBe("Drink coke");
+        });
+        it ("should return Drink beer", function() {
+            var result =whatCanIDrink(20);
+            expect(result).toBe("Drink beer");
+        });
+        it ("should return Drink whiskey", function() {
+            var result = whatCanIDrink(129);
+            expect(result).toBe("Drink whiskey");
         })
-    })
-})
+        it ("should return Sorry, I can't tell what to drink because that age is incorrect!", function() {
+            var result=whatCanIDrink(140);
+            expect(result).toBe("Sorry, I can't tell what to drink because that age is incorrect!");
+        })
+});
+});
